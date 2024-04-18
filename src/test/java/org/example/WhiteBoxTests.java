1b4a99207c53d2bd1d0b9fdf9c209b0737b2960a
Noah Drakes <noahdrakes@Noahs-MacBook-Air.local>
2024-04-17 22:59:33 -0400

    whitebox: a couple of tests for simpleMidi

--- src/test/java/org/example/WhiteBoxTests.java
+++ src/test/java/org/example/WhiteBoxTests.java
@@ -6,23 +6,28 @@ import org.junit.jupiter.api.Test;
 
 public class WhiteBoxTests implements SimpleMidiListener {
 
-    int globalpitch;
+    String globalpitch;
     int globalVelocity;
     int globalNoteChannel;
     int noteOnCount = 0;
     int noteOffCount = 0;
 
+    int noteStateChangeCount = 0;
+
     // A LIST OF THE LAST 20 RECENTLY PLAYED NOTES
-    String[] pitchesRecentlyPlayedList = new String[20];
+    String[] pitchesRecentlyPlayedList = new String[100];
+
+    // A LIST OF THE LAST 20 NOTE STATES
+    // 1 = ON, 0 = OFF
+    Integer[] noteStateRecenetlyPlayedList = new Integer[200];
     int getGlobalControllerChangeCount = 0;
 
-    int globalControllerChannel;
-    int globalControllerValue;
-    int globalControllerNumber;
-
-    String pitchBuffer;
 
     MidiBus mybus;
+
+    Note note;
+
+    ControlChange cc;
 
     static String[] pitchMap = new String[] {
             "C",
@@ -43,62 +48,118 @@ public class WhiteBoxTests implements SimpleMidiListener {
 
     @Test
     public void verifyPitchMidiOutTest() throws InterruptedException {
-        
+
         // TRY USING THROUGH PORT TO SIMULATE IN AND OUT TO READ BACK
         // MAYBE THE TINY MIDI KEYBOARD WILL WORK
 
         MidiBus midibus = new MidiBus(this,"Studio 68c", "Studio 68c");
         midibus.sendTimestamps(false);
-//        midibus.addMidiListener(this);
 
         midibus.sendNoteOn(0, 50, 127);
         Thread.sleep(5000);
         midibus.sendNoteOff(0, 50, 127);
-//        midibus.sendNoteOn(0, 15, 127);
 
+        String realPitch = pitchMap[50 % 12];
 
-//
         while (noteOnCount == 0){
             Thread.sleep(100);
         }
 
-        Assertions.assertTrue(globalVelocity == 127);
+        Assertions.assertTrue(note.name() == realPitch);
+    }
+
+    @Test
+    public void verifyNOteStateChangeTest() throws InterruptedException {
+        MidiBus midibus = new MidiBus(this,"Studio 68c", "Studio 68c");
+        midibus.sendTimestamps(false);
+
+        for (int i = 0; i < 20; i++){
+            midibus.sendNoteOn(0, 50, 127);
+            Thread.sleep(100);
+            midibus.sendNoteOff(0, 50, 127);
+            Thread.sleep(100);
+        }
+
+        Assertions.assertTrue(noteStateRecenetlyPlayedList[noteStateChangeCount - 1] == 0);
+    }
+
+    @Test
+    public void verifyVelocityChangeTest() throws InterruptedException {
+        MidiBus midibus = new MidiBus(this,"Studio 68c", "Studio 68c");
+        midibus.sendTimestamps(false);
+
+        for (int i = 0; i <= 50; i++){
+
+            midibus.sendNoteOn(0, 50, i);
+            Thread.sleep(100);
+            midibus.sendNoteOff(0, 50, i);
+            Thread.sleep(100);
+        }
+
+        // DETERMINE IF I NEED TO USE THIS GLOBAL VAR OR IF THIS GETTER FUNCTION WORKS
+//        Assertions.assertEquals(globalVelocity, 50);
+        Assertions.assertEquals(note.velocity, 50);
+    }
+
+    @Test
+    public void controllerChangeTest() throws InterruptedException {
+        MidiBus midibus = new MidiBus(this,"Studio 68c", "Studio 68c");
+        midibus.sendTimestamps(false);
+
+        midibus.sendNoteOn(0, 70, 127);
+        Thread.sleep(1000);
+        midibus.sendControllerChange(0, 1, 127);
+        Thread.sleep(4000);
+
+        midibus.sendControllerChange(0, 1, 0);
+        Thread.sleep(1000);
+        midibus.sendNoteOff(0, 70, 127);
+
+        Assertions.assertEquals(cc.channel, 0);
+        Assertions.assertEquals(cc.number, 1);
+        Assertions.assertEquals(cc.value, 0);
 
     }
 
 
+    // __________________________________________________________ //
+    // SIMPLE MIDI IMPLEMENTATION //
+    // __________________________________________________________ //
 
-    // SIMPLE MIDI IMPLEMENTATION
     @Override
     public void noteOn(int channel, int pitch, int velocity) {
 
-        Note note = new Note(channel,pitch,velocity);
+        note = new Note(channel,pitch,velocity);
         String note_pitch = note.name();
         pitchesRecentlyPlayedList[noteOnCount] = note_pitch;
+        noteStateRecenetlyPlayedList[noteStateChangeCount] = 1;
 
-        System.out.println("Note On -> pitch: " + note_pitch + " velocity: " + velocity + " channel " + channel);
-        globalVelocity = velocity;
-        globalpitch = pitch;
-        globalNoteChannel = channel;
+        // prints out note
+        System.out.println(note.toString());
 
         noteOnCount++;
+        noteStateChangeCount++;
     }
 
     @Override
     public void noteOff(int channel, int pitch, int velocity) {
-        System.out.println("Note Off: " + pitch + " with velocity " + velocity + " on channel " + channel);
-        globalVelocity = velocity;
-        globalpitch = pitch;
-        globalNoteChannel = channel;
+
+        Note note = new Note(channel,pitch,velocity);
+        String note_pitch = note.name();
+        noteStateRecenetlyPlayedList[noteStateChangeCount] = 0;
+
+        System.out.println("Note OFF -> pitch: " + note_pitch + " velocity: " + velocity + " channel " + channel);
+
         noteOffCount++;
+        noteStateChangeCount++;
     }
 
     @Override
     public void controllerChange(int channel, int number, int value) {
         System.out.println("Controller Change: " + number + " with value " + value + " on channel " + channel);
-        globalControllerChannel = channel;
-        globalControllerNumber = number;
-        globalControllerValue = value;
+
+        cc = new ControlChange(channel, number, value);
+        // might not need globals
     }
 
 }
